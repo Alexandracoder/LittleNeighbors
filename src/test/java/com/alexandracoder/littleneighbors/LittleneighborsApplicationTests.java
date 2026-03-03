02988878e8dc24c56ac81b6b5d480d06f0a00535
Alexandracoder <acasaviajes@gmail.com>
2026-03-03 11:40:45 +0100

    fix: wrong spelling in littleneighbors

---
+++ src/test/java/com/alexandracoder/littleneighbors/LittleneighborsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandracoder.littleneighbors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleneighborsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
