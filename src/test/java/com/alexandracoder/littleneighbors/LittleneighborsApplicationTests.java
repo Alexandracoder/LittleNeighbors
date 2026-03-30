49fcaaec687caeffb0ffc5cb44363b29fa019293
Alexandracoder <acasaviajes@gmail.com>
2026-03-30 03:05:18 +0200

    fix: add secret to yml file2

--- src/test/java/com/alexandracoder/littleneighbors/LittleneighborsApplicationTests.java
+++ src/test/java/com/alexandracoder/littleneighbors/LittleneighborsApplicationTests.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource
 class LittleneighborsApplicationTests {
 
 	@Test
