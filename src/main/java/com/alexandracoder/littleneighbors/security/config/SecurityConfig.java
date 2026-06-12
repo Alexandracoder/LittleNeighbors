66a4dac1cfacc8be1a5461e23b575c44c20cf7d0
Alexandracoder <acasaviajes@gmail.com>
2026-06-13 00:19:14 +0200

    fix: add .cors(...) to filterChain

--- src/main/java/com/alexandracoder/littleneighbors/security/config/SecurityConfig.java
+++ src/main/java/com/alexandracoder/littleneighbors/security/config/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(ex -> ex
