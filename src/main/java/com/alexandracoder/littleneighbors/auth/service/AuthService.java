45b96d5121d9abc9a0faaf0232fc90a1202bd1d6
Alexandracoder <acasaviajes@gmail.com>
2026-07-06 22:37:48 +0200

    Refactor email templates, add i18n support for English, Spanish and Valencian

--- src/main/java/com/alexandracoder/littleneighbors/auth/service/AuthService.java
+++ src/main/java/com/alexandracoder/littleneighbors/auth/service/AuthService.java
@@ -5,12 +5,17 @@ import com.alexandracoder.littleneighbors.profile.dto.UserProfileDTO;
 import com.alexandracoder.littleneighbors.shared.exceptions.UserAlreadyExistsException;
 import jakarta.validation.Valid;
 
+import java.util.Locale;
+
 public interface AuthService {
     void register(@Valid RegisterRequest request) throws UserAlreadyExistsException;
     AuthResponse login(AuthRequest request);
     UserProfileDTO getCurrentProfile(String email);
     AuthResponse reloadUserTokenFromRefresh(String refreshToken);
-    void sendWelcomeEmail(String email, String firstName);
-    void initiatePasswordReset(String email);
+
+    void sendWelcomeEmail(String email, String firstName, Locale locale);
+
+    void initiatePasswordReset(String email, Locale locale);
+
     void resetPassword(String token, String newPassword);
 }
